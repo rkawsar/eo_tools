a4b3df71fdea8d78bb7e721fc41af2e07f63063c
Riazuddin Kawsar <orck9102@yahoo.com>
2015-02-04 16:45:25 +0100

    Update L2_LAC_SST_preprocessing.py

--- oceancolor/L2_LAC_SST_preprocessing.py
+++ oceancolor/L2_LAC_SST_preprocessing.py
@@ -1,132 +1,217 @@
 #!/usr/bin/env python
 # Riazuddin Kawsar
 # 5th Feb 2015
-
-
-import os, sys, time, numpy, fnmatch
+import os
 from osgeo import gdal
-from osgeo.gdalconst import *
-import multiprocessing
+from osgeo import osr
 import matplotlib.pyplot as plt
+import numpy as np
+
+# def array2raster(newRasterfn,rasterOrigin,pixelWidth,pixelHeight,array):
+#   cols = array.shape[1]
+#   rows = array.shape[0]
+#   originX = rasterOrigin[0]
+#   originY = rasterOrigin[1]
+#   driver = gdal.GetDriverByName('GTiff')
+#   outRaster = driver.Create(newRasterfn, cols, rows, 1, gdal.GDT_Byte)
+#   outRaster.SetGeoTransform((originX, pixelWidth, 0, originY, 0, pixelHeight))
+#   outband = outRaster.GetRasterBand(1)
+#   outband.WriteArray(array)
+#   outRasterSRS = osr.SpatialReference()
+#   outRasterSRS.ImportFromEPSG(4326)
+#   outRaster.SetProjection(outRasterSRS.ExportToWkt())
+#   outband.FlushCache()
 
 
-#fillval_sst =
 
-
-
-
-startdate = '2014000'
-
-input_dir = '/media/Arc/eo_archive_proc/MOD09Q1/downloads/h18v03'
-output_dir = '/media/Arc/eo_archive_proc/MOD09Q1/output/h18v03'
 
 hdf_file = '/media/Num/eo_tools/testData/raster/A2014001000000.L2_LAC_SST'
 
-# function to read specific band
-def return_band(hdf_file, band_number):
-    
-    if band_number == 3:
-        print 'Get sst band (16-bit integer) ...'
-        hdf_file = 'HDF4_SDS:UNKNOWN:'+ hdf_file +':15'
-
-    elif band_number == 4:
-        print 'Get qual_sst band (8-bit integer) ...'
-        hdf_file = 'HDF4_SDS:UNKNOWN:'+ hdf_file +':16'
-
-    elif band_number == 8:
-        print 'Get l2_flags band (32-bit integer) ...'
-        hdf_file = 'HDF4_SDS:UNKNOWN:'+ hdf_file +':20'
-
-    else:
-        print 'Band number out of range ...'
-        sys.exit(1)
-
-
-    dataset = gdal.Open(hdf_file,gdal.GA_ReadOnly)
-    if dataset is None:
-        print "Could not open " + hdf_file
-        sys.exit(1)
-    geoTransform = dataset.GetGeoTransform()
-    proj = dataset.GetProjection()
-    ncols = dataset.RasterXSize
-    nrows = dataset.RasterYSize
-    nbands = dataset.RasterCount
-    print ncols, nrows, nbands
-
-    
-    rasterband = dataset.GetRasterBand(1)
-    NoDataValue = rasterband.GetNoDataValue()
-    print 'NoDataValue : ' + str (NoDataValue)
-    GetMinimum = rasterband.GetMinimum()
-    GetMaximum = rasterband.GetMaximum()
-
-    stats = rasterband.GetStatistics( True, True )
-    print "BandStatistics =  Min=%.3f, Max=%.3f, Mean=%.3f, StdDev=%.3f" % ( \
-                stats[0], stats[1], stats[2], stats[3] )
-    
-    type(rasterband)
-    rasterband = rasterband.ReadAsArray(0, 0, ncols, nrows)
-
-    if band_number == 3:
-        rasterband = rasterband.astype(numpy.uint16)
-    elif band_number == 4:
-        rasterband = rasterband.astype(numpy.uint8)
-    elif band_number == 8:
-        rasterband = rasterband.astype(numpy.uint32)
-    else:
-        print 'Band number out of range'
-        sys.exit(1)
-        
-    return rasterband,geoTransform,proj
-    dataset = None
-    rasterband = None
 
 
 
-# define the otput file
-def output_file(output_name,output_array,geoTransform,proj):
-    format = "GTiff"
-    driver = gdal.GetDriverByName( format )
-    outDataset = driver.Create(output_name,ncol,nrow,1,GDT_Int16)
-    outBand = outDataset.GetRasterBand(1)
-    outBand.WriteArray(output_array,0,0)
-    outBand.FlushCache()
-    outBand.SetNoDataValue(fillval_sst)
-    outDataset.SetGeoTransform(geoTransform )
-    outDataset.SetProjection(proj)
+# function to generate nodecode from lattitude and longitude
+def code_node_code(lat, lon):
+  
+  lat_p = "%03d" % (abs(lat))
+  lat_l = str(lat).split(".")[1]
+  lon_p = "%03d" % (abs(lon))
+  lon_l = str(lon).split(".")[1]
+
+  if lat > 0:
+    lat_code = 'W' + lat_p + '.' + lat_l
+  else:
+    lat_code = 'E' + lat_p + '.' + lat_l
+
+  if lon < 0:
+    lon_code = 'N' + lon_p + '.' + lon_l
+  else:
+    lon_code = 'S' + lon_p + '.' + lon_l
+
+  node_code = lat_code + '_' + lon_code
+  print node_code
+  return node_code
+  
 
 
 
-# finding all the hdf files in the mentioned root directories 
-def findfiles(input_dir):
-    toprocess = []
-    for root,dir,files in os.walk(input_dir):
-        for name in sorted(files):
-            if fnmatch.fnmatch(name,'*.hdf'):
-                if name[1:8] > startdate:
-                    #if name[1:8] == startdate:
-                    toprocess.append( os.path.join(root, name) )
-               #print os.path.join(root, name)
-    return toprocess
+
+def return_sst_band_arrays(hdf_file):
+  
+  ff_name = os.path.basename(hdf_file)
+  ff_name = os.path.splitext(ff_name)[0]
+  
+  hdf_file = gdal.Open(hdf_file,gdal.GA_ReadOnly)
+  subdatasets = hdf_file.GetSubDatasets()
+  variables = []
+  for subdataset in subdatasets:
+    variables.append(subdataset[1].split(" ")[1])
+  print 'available layers: ' + str(variables)
+
+
+  print 'reading longitude, latitude sst and qual_sst layers ...'
+  ds_lon = gdal.Open(subdatasets[0][0])
+  ds_lat = gdal.Open(subdatasets[1][0])
+  ds_sst = gdal.Open(subdatasets[2][0])
+  ds_quality = gdal.Open(subdatasets[3][0])
+
+
+  print 'coverting layers into numpy arrays ...'
+  longs = ds_lon.GetRasterBand(1).ReadAsArray()
+  lats = ds_lat.GetRasterBand(1).ReadAsArray()
+  sst = ds_sst.GetRasterBand(1).ReadAsArray()
+  quality = ds_quality.GetRasterBand(1).ReadAsArray()
+
+
+  print 'extracting meatadata ... '
+  metadata = hdf_file.GetMetadata()
+  DayOrNight = metadata['Day or Night'][0:1]
+  lat = (round(float(metadata['Scene Center Latitude']), 3))
+  lon = (round(float(metadata['Scene Center Longitude']), 3))
+  node_code = code_node_code(lat, lon)
+  out_dataset_name = ff_name + '_' + node_code + '_' + DayOrNight
+  print out_dataset_name
+  
+
+  # Scaling and NaN values
+  sst_nan = float(ds_sst.GetMetadataItem('bad_value_scaled'))
+  sst_intercept = float(ds_sst.GetMetadataItem('intercept'))
+  sst_slope = float(ds_sst.GetMetadataItem('slope'))
+  # Create mask
+  mask = sst == sst_nan
+  # Scale values
+  sst = sst_intercept + sst_slope * sst
+  # Apply mask
+  sst[mask] = np.nan
+
+  # free datasets
+  ds_lon, ds_lat, ds_sst, ds_quality = None, None, None, None
+
+  return lats, longs, sst, quality
 
 
 
-# define the product output name
-def product_output_name(output_dir,sst_name,product):
-    product_dir = os.path.join(output_dir,product)
-    product_output_name = sst_name +'.'+product+'.tif'
-    product_path_file = os.path.join(product_dir,product_output_name)
-    return product_path_file
 
-    
 
-rasterband,geoTransform,proj = return_band(hdf_file, 8)
+
+lats, longs, sst, quality = return_sst_band_arrays(hdf_file)
+gdal_dtype = gdal.GDT_Float32
+out_fn = '/media/Num/eo_tools/testData/raster/foo.tif'
+driver_name = 'GTiff'
+resolution_out = 1000
+ncols = sst.shape[1]
+nrows = sst.shape[0]
+n_px = ncols * nrows
+ul_x = longs.min()
+ul_y = lats.max()
+
+
+
+# Define projections (from and to)
+wgs84 = osr.SpatialReference()
+wgs84.ImportFromEPSG(4326)
+polar = osr.SpatialReference()
+polar.ImportFromEPSG(3031)
+
+#
+# ## Create SST daatset in memory
+# mem = gdal.GetDriverByName( 'MEM' )
+# ds_sst = mem.Create('', ncols, nrows, 1, gdal_dtype)
+# ds_sst.SetGeoTransform( (ul_x, 0.01, 0, ul_y, 0, 0.01) )
+# # Select output band
+# band_sst = ds_sst.GetRasterBand(1)
+# # Write array into output band
+# band_sst.WriteArray(sst)
+# # Set NoData value
+# band_sst.SetNoDataValue(np.nan)
+# # Set projection of output raster
+# ds_sst.SetProjection( wgs84.ExportToWkt() )
+#
+# ## REPROJECT DATA TO EPSG:3031
+# tx = osr.CoordinateTransformation (wgs84, polar)
+#
+# # GeoTransform vector of SST data
+# gt = [ float(x) for x in (ul_x, 0.01, 0, ul_y, 0, 0.01) ]
+# # Work out the boundaries of the new dataset in the target projection
+# (ulx, uly, ulz ) = tx.TransformPoint( gt[0], gt[3] )
+# (lrx, lry, lrz ) = tx.TransformPoint( gt[0] + gt[1] * ncols, gt[3] + gt[5] * nrows )
+#
+# # Now, we create an in-memory raster
+# driver = gdal.GetDriverByName( driver_name )
+# # The size of the raster is given the new projection and pixel spacing
+# # Using the values we calculated above. Also, setting it to store one band
+# # and to use Float32 data type.
+# ds_out = driver.Create(out_fn, int( abs(lrx - ulx) / resolution_out ), int( abs(uly - lry) / resolution_out ), 1, gdal_dtype )
+# # Calculate the new geotransform
+# out_gt = ( ulx, resolution_out, gt[2], uly, gt[4], -resolution_out )
+# # Set the geotransform
+# ds_out.SetGeoTransform( out_gt )
+# ds_out.SetProjection ( polar.ExportToWkt() )
+# # Perform the projection/resampling
+# res = gdal.ReprojectImage( ds_sst, ds_out, wgs84.ExportToWkt(), polar.ExportToWkt(), gdal.GRA_Bilinear )
+#
+
+
 
 
 '''
-# data printing
-imgplot2 = plt.imshow(rasterband)
+
+imgplot2 = plt.imshow(quality)
 plt.show()
 '''
 
+def output_file(output_name,output_array):
+    format = "GTiff"
+    driver = gdal.GetDriverByName( format )
+    outDataset = driver.Create(output_name, ncols, nrows, 1, GDT_Int16)
+    outBand = outDataset.GetRasterBand(1)
+    outBand.WriteArray(output_array,0,0)
+    outBand.FlushCache()
+    outBand.SetNoDataValue(0)
+    out.SetGeoTransform( (ul_x, 0.01, 0, ul_y, 0, 0.01) )
+    out.SetProjection( wgs84.ExportToWkt() )
 
+
+#output_file(out_fn, sst)
+
+'''
+## WRITE FILE
+
+# Get driver
+driver = gdal.GetDriverByName('GTiff')
+# Create output raster file
+out = driver.Create(out_fn, ncols, nrows, 1, gdal_dtype)
+# Set the affine transformation coefficients
+print ul_x, ul_y, ncols, nrows
+out.SetGeoTransform( (ul_x, 0.01, 0, ul_y, 0, 0.01) )
+# Select output band
+outband = out.GetRasterBand(1)
+# Write array into output band
+outband.WriteArray(sst)
+# Set NoData value
+outband.SetNoDataValue(np.nan)
+# Set projection of output raster
+out.SetProjection( wgs84.ExportToWkt() )
+# Flush cache
+outband.FlushCache()
+'''
