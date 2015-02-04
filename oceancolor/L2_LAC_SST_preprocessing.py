d2e109bba5a71a050357b23dbee627719aeda76c
Riazuddin Kawsar <orck9102@yahoo.com>
2015-02-04 11:35:01 +0100

    Update L2_LAC_SST_preprocessing.py

--- oceancolor/L2_LAC_SST_preprocessing.py
+++ oceancolor/L2_LAC_SST_preprocessing.py
@@ -2,7 +2,18 @@ import os, sys, time, numpy, fnmatch
 from osgeo import gdal
 from osgeo.gdalconst import *
 import multiprocessing
+import matplotlib.pyplot as plt
 
+
+#fillval_sst =
+
+
+
+
+startdate = '2014000'
+
+input_dir = '/media/Arc/eo_archive_proc/MOD09Q1/downloads/h18v03'
+output_dir = '/media/Arc/eo_archive_proc/MOD09Q1/output/h18v03'
 
 hdf_file = '/media/Num/eo_tools/testData/raster/A2014001000000.L2_LAC_SST'
 
@@ -12,14 +23,17 @@ def return_band(hdf_file, band_number):
     if band_number == 3:
         print 'Get sst band (16-bit integer) ...'
         hdf_file = 'HDF4_SDS:UNKNOWN:'+ hdf_file +':15'
+
     elif band_number == 4:
         print 'Get qual_sst band (8-bit integer) ...'
         hdf_file = 'HDF4_SDS:UNKNOWN:'+ hdf_file +':16'
+
     elif band_number == 8:
         print 'Get l2_flags band (32-bit integer) ...'
         hdf_file = 'HDF4_SDS:UNKNOWN:'+ hdf_file +':20'
+
     else:
-        print 'Band number out of range'
+        print 'Band number out of range ...'
         sys.exit(1)
 
 
@@ -36,17 +50,14 @@ def return_band(hdf_file, band_number):
 
     
     rasterband = dataset.GetRasterBand(1)
-    GetNoDataValue = rasterband.GetNoDataValue()
+    NoDataValue = rasterband.GetNoDataValue()
+    print 'NoDataValue : ' + str (NoDataValue)
     GetMinimum = rasterband.GetMinimum()
     GetMaximum = rasterband.GetMaximum()
 
-    print   'MaxValue: ' + str(GetMaximum) + ' , ' \
-          + 'MinValue: ' + str(GetMinimum) + ' , ' \
-            'NoDataValue: ' + str(GetNoDataValue)
-
-    # stats = rasterband.GetStatistics (true, true)
-    # print "[ STATS ] =  Minimum=%.3f, Maximum=%.3f, Mean=%.3f, StdDev=%.3f" % ( \
-    #            stats[0], stats[1], stats[2], stats[3] )
+    stats = rasterband.GetStatistics( True, True )
+    print "BandStatistics =  Min=%.3f, Max=%.3f, Mean=%.3f, StdDev=%.3f" % ( \
+                stats[0], stats[1], stats[2], stats[3] )
     
     type(rasterband)
     rasterband = rasterband.ReadAsArray(0, 0, ncols, nrows)
@@ -64,8 +75,53 @@ def return_band(hdf_file, band_number):
     return rasterband,geoTransform,proj
     dataset = None
     rasterband = None
- 
 
-return_band(hdf_file, 3)
-return_band(hdf_file, 4)
-return_band(hdf_file, 8)
+
+
+# define the otput file
+def output_file(output_name,output_array,geoTransform,proj):
+    format = "GTiff"
+    driver = gdal.GetDriverByName( format )
+    outDataset = driver.Create(output_name,ncol,nrow,1,GDT_Int16)
+    outBand = outDataset.GetRasterBand(1)
+    outBand.WriteArray(output_array,0,0)
+    outBand.FlushCache()
+    outBand.SetNoDataValue(fillval_sst)
+    outDataset.SetGeoTransform(geoTransform )
+    outDataset.SetProjection(proj)
+
+
+
+# finding all the hdf files in the mentioned root directories 
+def findfiles(input_dir):
+    toprocess = []
+    for root,dir,files in os.walk(input_dir):
+        for name in sorted(files):
+            if fnmatch.fnmatch(name,'*.hdf'):
+                if name[1:8] > startdate:
+                    #if name[1:8] == startdate:
+                    toprocess.append( os.path.join(root, name) )
+               #print os.path.join(root, name)
+    return toprocess
+
+
+
+# define the product output name
+def product_output_name(output_dir,sst_name,product):
+    product_dir = os.path.join(output_dir,product)
+    product_output_name = sst_name +'.'+product+'.tif'
+    product_path_file = os.path.join(product_dir,product_output_name)
+    return product_path_file
+
+    
+
+rasterband,geoTransform,proj = return_band(hdf_file, 8)
+
+
+'''
+# data printing
+imgplot2 = plt.imshow(rasterband)
+plt.show()
+'''
+
+
