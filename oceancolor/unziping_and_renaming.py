cb3e2292a80cb8e93d848cb78f1a2997324c4c7c
Riazuddin Kawsar <orck9102@yahoo.com>
2015-02-09 12:49:52 +0100

    Create unziping_and_renaming.py

---
+++ oceancolor/unziping_and_renaming.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# Riazuddin Kawsar
+# 5th Feb 2015
+
+# unzip the downlodeded file and rename the raster files as per 'SST' and 'NSST'
+
+import os, fnmatch, bz2, fnmatch
+from osgeo import gdal
+
+
+input_dir = '/media/Num/wrk_dir/oceancolour/L2_LAC_SST'
+
+for(dirpath,dirnames,files)in os.walk(input_dir):
+    
+    for file in files:
+        if fnmatch.fnmatch(file,'*.bz2'):
+            filepath = os.path.join(dirpath,file)
+            print filepath
+            
+            zipfile = bz2.BZ2File(filepath)
+            data = zipfile.read()
+            newfilepath = filepath[:-7]
+            open(newfilepath, 'wb').write(data)
+            
+            hdf_file = gdal.Open(newfilepath,gdal.GA_ReadOnly)
+            metadata = hdf_file.GetMetadata()
+            DayOrNight = metadata['Day or Night'][0:1]
+
+            if DayOrNight =='N':
+                new_newfilepath = newfilepath + 'NSST'
+            else:
+                new_newfilepath = newfilepath + 'SST'
+            print new_newfilepath
+                
+            os.rename(newfilepath, new_newfilepath)
+            os.remove(filepath)
