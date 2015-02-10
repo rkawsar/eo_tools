f7efc7b5566f385d75ca659bdfefdf4ed7be79f7
Riazuddin Kawsar <orck9102@yahoo.com>
2015-02-10 20:28:22 +0100

    Update and rename spatial_search_and_download_MODIS_L2_data.py to spatial_search_download_L2.py

--- oceancolor/spatial_search_download_L2.py
+++ oceancolor/spatial_search_download_L2.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 
-# python script to download SST data for the year 2012
 
+# python script to download SST data for the year 2012
 import re
 import os
 import urllib, urllib2
 import datetime, time
 
+# creating limit to reduce the data binning size [n, w, e, s]
+lim_A = [ -90, -180, -60, 90 ]
+lim_B = [ -90, -59, 80, 90 ]
+lim_C = [ -90, 79, 180, 90 ]
+
+print 'working for limit [n, w, e, s] ' + str(lim_A)
+
+
 pram = 'SST'
 OUT_DIR = "C:\R.Kawsar\WORKSPACE\Data\oceancolour"
 
-year = '2012'
+year = '2014'
 day = '01'
 month = '01'
 
@@ -19,10 +27,9 @@ user_TInput = month + '/' + day + '/' + year[2:]
 d1 = datetime.datetime.strptime(user_TInput, "%m/%d/%y")
 d0 = datetime.datetime.strptime('01/01/70', "%m/%d/%y")
 start_day =(d1 - d0).days
-end_day = start_day + 365 
+end_day = start_day + 10
 
-#for i in range(start_day, end_day):
-#    print i
+print start_day, end_day
 
 
 INPUTS = {
@@ -38,9 +45,9 @@ INPUTS = {
     'frc': 0,
     'set': 10,
     'n': 50,
-    'w': -100,
-    'e': -95,
-    's': 40,}
+    'w': -97,
+    'e': -96,
+    's': 49,}
 
 URL_FIND = "http://oceancolor.gsfc.nasa.gov/cgi/browse.pl/"
 URL_GET = "http://oceandata.sci.gsfc.nasa.gov/cgi/getfile/%s"
@@ -54,11 +61,22 @@ def http_post(url, args):
     return page
 
 if __name__ == "__main__":
-    page = http_post(URL_FIND, INPUTS)
-    scenes = re.findall(PATT, page)
-    scenes = list(set(scenes))
-    for scene in scenes:
-        filename = scene + ".bz2"
-        outpath = os.path.join(OUT_DIR, filename)
-        url = URL_GET % filename
-        urllib.urlretrieve(url, outpath)
+    for i in range(start_day, end_day):
+        
+        INPUTS[ 'day' ] = i
+        INPUTS[ 'dnm' ] = 'D'
+        INPUTS[ 'n' ] = lim_A[0]
+        INPUTS[ 'w' ] = lim_A[1]
+        INPUTS[ 'e' ] = lim_A[2]
+        INPUTS[ 's' ] = lim_A[3]
+
+        page = http_post(URL_FIND, INPUTS)
+        scenes = re.findall(PATT, page)
+        scenes = list(set(scenes))
+        
+        for scene in scenes:
+            filename = scene + ".bz2"
+            print filename
+            #outpath = os.path.join(OUT_DIR, filename)
+            #url = URL_GET % filename
+            #urllib.urlretrieve(url, outpath)
