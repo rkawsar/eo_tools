875286c5883da9c95e49c31cfb5b25019490c06b
Riazuddin Kawsar <orck9102@yahoo.com>
2015-03-31 21:18:26 +0200

    Update Stratified random sampling.py

--- sampling/Stratified random sampling.py
+++ sampling/Stratified random sampling.py
@@ -1,31 +1,57 @@
-#Stratified random sampling script
+
+# Stratified random sampling script
+# source: http://arizona.openrepository.com/arizona/bitstream/10150/311667/1/azu_etd_13118_sip1_m.pdf
+
 import arcpy
 arcpy.CheckOutExtension("spatial")
-rr = raw_input("Give this run a number") #This run number eliminates the need to go back and delete files if executing this script multiple times
+
+#This run number eliminates the need to go back and delete files if executing this script multiple times
+rr = raw_input("Give this run a number")
 r = str(rr)
 print "This is now run " + r
 raw_input("Press enter to continue")
+
 #Reset these parameters accordingly. Remember to use \\ instead of \ in file paths
-d = "I:\\accuracy_assessment\\" #base directory, needs an "outputs" folder and a "scratch" folder and a "final_merge" folder
-training_poly = "I:\\training_sites.shp" #training polygons shapefile
-POINTS = "50" #number of points
-landcover_in = "I:\\post_process_r7\\r7_final" #input land cover map
-rand_space = "90" #Minimum distance between randomized points in meters. Can be 0 if desired
-lc_count = 10 #Highest land cover class grid code. Land cover raster must contain all sequential values between 1 and lc_count.
+#base directory, needs an "outputs" folder and a "scratch" folder and a "final_merge" folder
+d = "I:\\accuracy_assessment\\" 
+
+#training polygons shapefile
+training_poly = "I:\\training_sites.shp"
+ #number of points
+POINTS = "50"
+
+#input land cover map
+landcover_in = "I:\\post_process_r7\\r7_final"
+
+#Minimum distance between randomized points in meters. Can be 0 if desired
+rand_space = "90"
+
+#Highest land cover class grid code. Land cover raster must contain all sequential values between 1 and lc_count.
+lc_count = 10 
+
 #Variables. Leave these unmodified:
 outputs = d + "outputs"
 focal_stats_run_img = d + "scratch\\focal_stats_run" + r + ".img"
 reclass_run_img = d + "scratch\\reclass_run" + r + ".img"
 raster_point_run_shp = d + "scratch\\raster_point_run" + r + ".shp"
 value_to_point_run_shp = d + "scratch\\value_to_point_run" + r + ".shp"
+
 #to convert points to squares:
 point_merge_buffer_run_shp = d + "scratch\\point_merge_buffer_run" + r + ".shp"
 point_erase_run_shp = d + "scratch\\point_erase_run" + r + ".shp"
 mergestring = ""
 run_point_merge_shp = d + "final_merge\\run" + r + "_point_merge.shp"
-assessment_squares_run_shp = d + "final_merge\\assessment_squares_run" + r + ".shp" #this is the final output of the stratified random sampling
-# Process: Focal Statistics- targets center pixels of homogenous land cover arcpy.gp.FocalStatistics_sa(landcover_in, focal_stats_run_img, "Rectangle 3 3 CELL", "VARIETY", "DATA") print "focal stats finished"
-# Process: Reclassify- eliminates all pixels not surrounded by homogenous lc arcpy.gp.Reclassify_sa(focal_stats_run_img, "VALUE", "1 1", reclass_run_img, "NODATA")
+
+#this is the final output of the stratified random sampling
+assessment_squares_run_shp = d + "final_merge\\assessment_squares_run" + r + ".shp" 
+
+# Process: Focal Statistics- targets center pixels of homogenous land cover 
+# arcpy.gp.FocalStatistics_sa(landcover_in, focal_stats_run_img, 
+# "Rectangle 3 3 CELL", "VARIETY", "DATA") print "focal stats finished"
+
+# Process: Reclassify- eliminates all pixels not surrounded by homogenous 
+# lc arcpy.gp.Reclassify_sa(focal_stats_run_img, "VALUE", "1 1", reclass_run_img, "NODATA")
+
 print "reclassify done"
 # Process: Raster to Point
 arcpy.RasterToPoint_conversion(reclass_run_img, raster_point_run_shp, "VALUE")
@@ -39,7 +65,10 @@ print "extract value to point done"
 Thesis Overview 91
 ￼
 x = str(intx)
-class_run_all_shp = d + "scratch\\class" + x + "_run" + r + "_all.shp" arcpy.Select_analysis(value_to_point_run_shp, class_run_all_shp, "\"RASTERVALU\" = " + x) #grabs points of
+#grabs points of
+
+class_run_all_shp = d + "scratch\\class" + x + "_run" + r + "_all.shp" arcpy.Select_analysis(value_to_point_run_shp, class_run_all_shp, "\"RASTERVALU\" = " + x) 
+
 each land cover class
 print "class " + x + "has been selected"
 out = "class" + x + "_run" + r + "_points"
