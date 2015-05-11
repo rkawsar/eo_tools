9ef975add9d5560ecbdb974c3c30da4e7c7f65cc
Riazuddin Kawsar <orck9102@yahoo.com>
2015-05-11 16:10:11 +0200

    Update MAGIC_ypm_and_statistics_release.py

--- geoprocessing_raster/MAGIC_ypm_and_statistics_release.py
+++ geoprocessing_raster/MAGIC_ypm_and_statistics_release.py
@@ -2,36 +2,53 @@
 # coding: utf-8
 # Author: Riazuddin Kawsar
 # E-mail: r.kawsar@spatial-business-integration.com
-# Date: 7-May 2015
-
+# Date: 11-May 2015
+# Vegiation Index Computation (for 250 meters pixel)
+# using MOD09Q1 and MOD09A1
 '''
 scrpit description: Uses the Natural Jenks Breaks Algorithom to get the break intervals
 and then reclass the Jenks classes (equal interval between 2nd and 9th item in the breaks)
 and then use the reclass Jenks to classify the input image and 
 '''
 
+
+
 import numpy, math
-from osgeo import gdal  
+from osgeo import gdal
+import ogr
 import sys, os, fnmatch, csv, time
 import matplotlib.pylab as plt
 from operator import itemgetter
 import xml.etree.cElementTree as ET
 import multiprocessing
 
+
 # input variable
-site_name = 'Test' # Lechfeld, Aschersleben, Oschersleben , Steinburg , Limburgerhof, Elmshorn, Grossalsleben
+site_name = 'Aschersleben' # Lechfeld, Aschersleben, Oschersleben , Steinburg , Limburgerhof, Elmshorn, Grossalsleben, Laubach
+crop_to_process = 'ww' # ww, wrs, cr, all
+submission_version = 1
+
 
 
 # parmanent for MAGIC 2 project
 startTime = time.time()
 project_path = '/media/SBIProject/150301_BASF_MAGIC/Germany/site_specific_analysis'
 in_rster_dir= os.path.join(project_path, site_name, 'FA')
+in_shp_path = os.path.join(project_path, site_name, 'shpfiles')
+
 numClass = 10
-#The interval values to classify  
 #classification_values = [0.00001, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-#The value assigned to each interval
 classification_output_values = [1,2,3,4,5,6,7,8,9,10]
 fill_value = 0
+
+
+def find_shp_files(input_dir):
+    toprocess = []
+    for root,dir,files in os.walk(input_dir):
+        for name in sorted(files):
+            if fnmatch.fnmatch(name, '*_utm_to_clip.shp'): 
+                toprocess.append( os.path.join(root, name) )
+    return toprocess
 
 def findfiles(input_dir):
     toprocess = [] 
@@ -69,6 +86,7 @@ def get_raster_array(in_raster, band_number):
         dataList.append(numarray[x][y])
     dataList = remove_values_from_list(dataList, noDataValue)
     return dataList
+
 
 def get_jenks_breaks (dataList, numClass):
     dataList.sort()
@@ -124,6 +142,7 @@ def get_jenks_breaks (dataList, numClass):
       countNum -= 1
     return kclass
 
+
 def reclass_jenks_class(jenks_class, classes):
     min_value = jenks_class[1]
     max_value = jenks_class[9]
@@ -132,7 +151,7 @@ def reclass_jenks_class(jenks_class, classes):
     reclass[0] = 0.00001
     reclass[10] = 1
     return reclass
-        
+
 def gather_raster_stat(band):
     max_value = band.GetMaximum()
     min_value = band.GetMinimum()
@@ -147,7 +166,6 @@ def gather_raster_stat(band):
 def classify_raster(class_in_values, class_out_values, in_raster, out_name):
     dataset, band, ncol, nrow, x_block, y_block = return_raster(in_raster, 1)
     max_value, min_value = gather_raster_stat(band)
-    # create a temporary raster to store the result
     name = os.path.basename(in_raster)
     format = "GTiff"
     driver = gdal.GetDriverByName( format )
@@ -183,7 +201,6 @@ def raster_class_stat_dict(in_raster):
     xsize = in_raster.RasterXSize
     ysize = in_raster.RasterYSize
     bands = in_raster.RasterCount
-    # process the raster
     for i in xrange(1, bands + 1):
         band_i = in_raster.GetRasterBand(i)
         raster = band_i.ReadAsArray()
@@ -210,6 +227,7 @@ def raster_class_stat_dict(in_raster):
     #print 'total number useable pixels: ' + str(sums)
     sums = None
     return count
+
 
 def manage_list(jenks_class, count_dic):
     vvi = 0.50
@@ -240,6 +258,8 @@ def manage_list(jenks_class, count_dic):
                 k_list.append(x)
     return k_list
 
+
+
 def create_list_to_xml(k_list, xml_out):
     csvimport = ET.Element("csvimport")
     for i in range(len(k_list)):
@@ -248,6 +268,7 @@ def create_list_to_xml(k_list, xml_out):
         i_value = ET.SubElement(row,"value")
         i_vi = ET.SubElement(row,"vi")
         i_count = ET.SubElement(row,"pixel_count")
+        
         i_class.text = str(k_list[i][0])
         i_vi.text = str(k_list[i][2])
         i_value.text = str(k_list[i][1])
@@ -256,40 +277,89 @@ def create_list_to_xml(k_list, xml_out):
     tree.write(xml_out)
     return tree
 
+
+def extract_crop_type_info(in_shp_path):
+    crop_list = []
+    for inshape in find_shp_files(in_shp_path):
+        ds = ogr.Open(inshape)
+        lyr = ds.GetLayer(0)
+        lyr.ResetReading()
+        ft = lyr.GetNextFeature()
+        while ft:
+            field_name = ft.GetFieldAsString('field_name')
+            field_name = field_name.replace('_', '')
+            field_name = field_name.replace('.', '')
+            crop_type = ft.GetFieldAsString('crop')
+            x = [field_name, crop_type]
+            if x not in crop_list:
+                crop_list.append(x)
+            ft = lyr.GetNextFeature()
+        ds = None
+    return crop_list
+
+
+def return_field_crop_info(field_name, crop_list):
+    for i in crop_list:
+        if field_name == i[0]:
+            x = i[1]
+    return x
+
+
+def uniq(i_list):
+    last = object()
+    for item in i_list:
+        if item == last:
+            continue
+        yield item
+        last = item
+
 # accumulating the processing 
 def doprocess(raster):
     filename = raster.split('/')[-1]
     out_dir = raster.replace(filename, '')
     print 'Reclassification Process Started for ... ' + filename
     filename = filename.replace('.tif', '')
-    out_file = filename + '_fypm.tif'
-    out_file = os.path.join(out_dir, out_file)
-    xml_out_file = os.path.join(out_dir, out_file + '.xml')
-    if os.path.exists(out_file):
-        os.remove(out_file)
-    if os.path.exists(xml_out_file):
-        os.remove(xml_out_file)
-    datalist = get_raster_array(raster, 1)
-    jenks_class = get_jenks_breaks (datalist, numClass)
-    re_jenks_class = reclass_jenks_class(jenks_class, numClass)
-    classify_raster(re_jenks_class, classification_output_values, raster, out_file)
-    count = raster_class_stat_dict(out_file)
-    complete_list = manage_list(re_jenks_class, count)
-    xml_file = create_list_to_xml(complete_list, xml_out_file)
+    filename = filename.split('_')[0]
+    filename = filename.replace('_', '')
+    filename = filename.replace('.', '')
+    siteCode = site_name.lower()[0:3]
+    crop_list = extract_crop_type_info(in_shp_path)
+    crop_list = uniq(crop_list)
+    crop = return_field_crop_info(filename, crop_list)
+    if crop == crop_to_process:
+        print crop
+        out_file = 'fypm_' + str(submission_version) + '_' + crop + '_' + siteCode + '_' + filename + '.tif'
+        print out_file
+        out_file = os.path.join(out_dir, out_file)
+        xml_out_file = os.path.join(out_dir, out_file + '.xml')
+        xml_out_file = xml_out_file.replace('.tif', '')
+        if os.path.exists(out_file):
+            os.remove(out_file)
+        if os.path.exists(xml_out_file):
+            os.remove(xml_out_file)
+        datalist = get_raster_array(raster, 1)
+        jenks_class = get_jenks_breaks (datalist, numClass)
+        re_jenks_class = reclass_jenks_class(jenks_class, numClass)
+        print re_jenks_class
+        classify_raster(re_jenks_class, classification_output_values, raster, out_file)
+        count = raster_class_stat_dict(out_file)    
+        complete_list = manage_list(re_jenks_class, count)
+        xml_file = create_list_to_xml(complete_list, xml_out_file)
+
 
 def main(in_rster_dir):
     input_dir = in_rster_dir
     filelist = findfiles(input_dir)
     pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
-    # pool = multiprocessing.Pool(processes=4)
+    #pool = multiprocessing.Pool(processes=1)
     pool.map(doprocess,filelist)
     pool.close()
     pool.join()
     print 'Using ' +str(multiprocessing.cpu_count())+' cores.'
+    
 
 if __name__ == "__main__":
     main(in_rster_dir)
     endTime = time.time()
     print '\n\nThe script took ' +str(endTime - startTime)+ ' seconds'
 
- 
